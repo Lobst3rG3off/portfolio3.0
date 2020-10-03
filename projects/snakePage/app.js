f5ea31be3a819be645202482317ce143bf5d71c0
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-10-02 20:22:49 -0400

    adding projects before the modal

---
+++ projects/snakePage/app.js
@@ -0,0 +1,49 @@
+import { update as updateSnake, draw as drawSnake, snakeSpeed, getSnakeHead, snakeIntersection  } from './snake.js'
+import {update as updateFood, draw as drawFood} from './food.js'
+import {outsideGrid} from './grid.js'
+
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById('game-board');
+
+
+function main(currentTime) {
+
+    if (gameOver){
+        if (confirm('You Lose! Hit OK to Play Again!')) {
+            window.location = '/'
+        }
+        return
+    }
+
+
+
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+     if (secondsSinceLastRender < 1 / snakeSpeed) return
+
+    lastRenderTime = currentTime
+
+update()
+draw()
+
+
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+ updateSnake()
+ updateFood()
+ checkDeath()
+}
+
+function draw() {
+    gameBoard.innerHTML = ''
+ drawSnake(gameBoard)
+ drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
